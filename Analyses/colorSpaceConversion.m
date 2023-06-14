2ebe78731130eed5ac8ca867c1283f13f9b89a4e
da5nsy <dannygarside@outlook.com>
2023-06-14 13:00:35 -0400

    typo;

--- Analyses/colorSpaceConversion.m
+++ Analyses/colorSpaceConversion.m
@@ -11,7 +11,7 @@ function indexedLocation = colorSpaceConversion(requestedHueAngles)
 % load('C:\Users\cege-user\Documents\MacaqueColorCategories\Analyses\combined\combined_TCC-0att_fullremap-workspace_230510.mat',...
 %     'x');
 
-x = readmatrix('combined_TCC-0att_fullremap-x_230510.csv')
+x = readmatrix('combined_TCC-0att_fullremap-x_230510.csv');
 
 %%
 
