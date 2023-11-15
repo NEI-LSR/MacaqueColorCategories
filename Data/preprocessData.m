1df553a66a00538b41368a4f9e2eece5d1c2f40f
Danny Garside <dannygarside@outlook.com>
2023-11-15 13:52:53 -0500

    test data preprocessing

---
+++ Data/preprocessData.m
@@ -0,0 +1,20 @@
+clear, clc, close all
+
+cd('/Volumes/bc6/PROJECTS/CausalGlobs/analysis')
+
+cleandata = analysis('dirname',{'210422_135417_Pollux', '211012_124119_Pollux'}, 'heatmapfig', true, 'makefigs', false);
+save('/Volumes/bc6/PROJECTS/MacaqueColorCategories/Data/210422--211012_Pollux_data.mat','cleandata')
+
+cleandata = analysis('dirname',{'210517_090630_Castor', '211108_090705_Castor'}, 'heatmapfig', true, 'makefigs', false);
+save('/Volumes/bc6/PROJECTS/MacaqueColorCategories/Data/210517--211108_Castor_data.mat','cleandata')
+
+cleandata = analysis('dirname',{'210428_125912_Buster', '210609_124628_Buster'}, 'heatmapfig', true, 'makefigs', false);
+save('/Volumes/bc6/PROJECTS/MacaqueColorCategories/Data/210428--210609_Buster_data.mat','cleandata')
+
+cleandata = analysis('dirname',{'220322_091149_Morty', '220823_081207_Morty'}, 'heatmapfig', true, 'makefigs', false);
+save('/Volumes/bc6/PROJECTS/MacaqueColorCategories/Data/220322--220823_Morty_data.mat','cleandata')
+
+
+%%
+clc
+cleandata = analysis('dirname',{'210422_135417_Pollux'}, 'heatmapfig', true, 'choicefixfig', true, 'makefigs', false);
--- Data/readme.md
+++ Data/readme.md
@@ -2,17 +2,7 @@
 Raw data available in the Causal Globs repo (`CausalGlobs/data`).
 
 ## Preprocessed data
-Data extracted to here by running `CausalGlobs/Analyses/analysis.m` as:
-
-```
-cleandata = analysis('dirname',{'210422_135417_Pollux', '211012_124119_Pollux'}, 'heatmapfig', true, 'makefigs', false);
-
-cleandata = analysis('dirname',{'210517_090630_Castor', '211108_090705_Castor'}, 'heatmapfig', true, 'makefigs', false)
-
-cleandata = analysis('dirname',{'210428_125912_Buster', '210609_124628_Buster'}, 'heatmapfig', true, 'makefigs', false)
-
-cleandata = analysis('dirname',{'220322_091149_Morty', '220823_081207_Morty'}, 'heatmapfig', true, 'makefigs', false)
-```
+Data extracted to here by running `preprocessData.m` which calls `CausalGlobs/Analyses/analysis.m` 
 
 TODO Make it so that it incorporates cue/choice locations, and trial timing
 
