fcb538a42295c2516de4ddcfc92bb89bcc8a838b
da5nsy <dannygarside@outlook.com>
2023-11-15 17:13:24 -0500

    bootstrap stuff

--- Analyses/bootstrap.m
+++ Analyses/bootstrap.m
@@ -9,7 +9,7 @@ rng(0)
 bootstrap_ = true;
 
 for i = 1:100
-    rn = i; % use i as a random number generator so that new starting values are used each time
+    rn = i; % use i as a random number generator so that new starting values are used each
     data = combineData_mat(dirname,rn,bootstrap_);
 
     data.trialdata.nBig = 64;
@@ -19,4 +19,25 @@ for i = 1:100
     [~,~,~,nll(i).single_og,~]      = ParameterEstimator_caller(rn,data,'single-og');
     [~,~,~,nll(i).single_ssnu,~]    = ParameterEstimator_caller(rn,data,'single-ssnu');
 
+    close all
+
 end
+
+%% Either use the nll from above, or reload everything in from the saved model fits
+
+d = dir('*single-og*');
+for i = 1:length(d)
+    load(d(i).name)
+    nll_reloaded(i,1) = nll_x;
+end
+
+d = dir('*single-ssnu*');
+for i = 1:length(d)
+    load(d(i).name)
+    nll_reloaded(i,2) = nll_x;
+end
+
+figure, hold on
+plot(nll_reloaded(:,1),'DisplayName','Offset Gaussian')
+plot(nll_reloaded(:,2),'DisplayName','SSNU')
+legend('Location','best')
