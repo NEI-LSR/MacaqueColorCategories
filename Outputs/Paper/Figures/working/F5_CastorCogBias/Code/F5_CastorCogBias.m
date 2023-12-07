8e3ae3609f647c9bece2c8c47724bf187fb0c4ae
da5nsy <dannygarside@outlook.com>
2023-12-07 20:35:28 +0000

    confirm F5 operational

--- Outputs/Paper/Figures/working/F5_CastorCogBias/Code/F5_CastorCogBias.m
+++ Outputs/Paper/Figures/working/F5_CastorCogBias/Code/F5_CastorCogBias.m
@@ -6,20 +6,17 @@ rng(0)
 
 %%
 
-% Add path to required script
-addpath(genpath('../../../../../../Analyses/'))
-
-load('../../../../../../Analyses/TCCModels/Castor/211108_090705_Castor/220517--211108_Castor_TCC-FreeSimilarityMatrix-workspace_230225_x.mat')
-
 % TODO write in options for other levels of reproduction
+
+addpath(genpath('../../../../../../Analyses/'))
+load('../../../../../../Analyses/TCCModels/Castor/211108_090705_Castor/220517--211108_Castor_TCC-FreeSimilarityMatrix-workspace_230225_x.mat')
 
 %%
 
 categoryCenter = 18;
 plotSimilarityMatrix(x,'18','../',categoryCenter)
-
-% plotSimilarityMatrix(x,'','../')
+plotSimilarityMatrix(x,'','../')
 
 %%
 
-% plotbar_NLL_AIC_BIC('Castor','../')
+plotbar_NLL_AIC_BIC('Castor','../')
