e4daf6400331a79b039a6e6feb6e18ecbfef7d4a
da5nsy <dannygarside@outlook.com>
2023-12-11 19:15:03 +0000

    PTB

---
+++ Analyses/PTB/FToRatio.m
@@ -0,0 +1,23 @@
+%Source: https://github.com/Psychtoolbox-3/Psychtoolbox-3/blob/5ca36aa135a0bf30dda0ea47019ad6dfad327b7d/Psychtoolbox/PsychColorimetric/FToRatio.m
+
+function ratio = FToRatio(F)
+% ratio = FToRatio(F)
+%
+% This is related to Lab calculations.
+%
+% 10/10/93    dhb   Converted from CAP C code.
+
+% Find sizes and allocate
+[m,n] = size(F);
+ratio = zeros(m,n);
+
+% Compute according to the range
+hIndex = find( F > 0.206893);
+lIndex = find( F <= 0.206893);
+if (~isempty(hIndex))
+  ratio(hIndex) = F(hIndex).^3.0;
+end
+if (~isempty(lIndex))
+  ratio(lIndex) = (F(lIndex)-(16.0/116.0)*ones(length(lIndex),1))/7.787;
+end
+  
