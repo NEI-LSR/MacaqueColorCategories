d6031b456d685b629fea0fe46b0c9f1e9e2008f9
da5nsy <dannygarside@outlook.com>
2023-12-11 13:10:03 +0000

    clarify the code for F3ace

--- Outputs/Paper/Figures/working/F3_TCCModel/Code/F3ace_TCCModel_OffsetGaussian.m
+++ Outputs/Paper/Figures/working/F3_TCCModel/Code/F3ace_TCCModel_OffsetGaussian.m
@@ -1,5 +1,21 @@
 %% Generate simulated data based on a offset gaussian model
 
+% This code generates a simulated data set that when analyzed with a 
+% mixture model captures the average pattern of results made by macaque 
+% monkeys in a match-to-sample color-matching task. 
+%
+% The code fits a mixture model (Zhang and Luck, 2008, Bae et al, 2015)
+% to behavioral data, and then simulates data to match the biases found in 
+% that data.
+%
+% It then fits both a mixture model and a modified Target 
+% Confusability Competition (TCC) model (Schurgin et al, 2020) to that
+% simulated data and plots visualisations of both.
+%
+% The simulated data set captures the results of an agent with a true 
+% cognitive bias when assessed using a paradigm that uniformly samples 
+% colors from a truly uniform perceptual color space.
+
 clear, clc, close all
 
 rng(0)
@@ -13,18 +29,18 @@ rng(0)
 
 % fromRawData:               % Generate figures from the raw data (slowest)
 % fromPreProcessedData:      % Generate figures from the pre-processed data
+% fromModelOutput:           % Generate figures from the model outputs only (fastest)
 
 AnalysisDepth = 'fromPreProcessedData';
 
-%%
+%% Add path to required scripts
 
-% Add path to required script
 addpath(genpath('../../../../../../Analyses/'))
 
 DataDir = ['..',filesep,'..',filesep,'..',filesep,'..',filesep,'..',filesep,'..',filesep,...
     'Data'];
 
-%%
+%% Load data
 
 if strcmp(AnalysisDepth,'fromRawData')
     addpath(genpath('../../../../../../Data/'))
@@ -32,41 +48,48 @@ if strcmp(AnalysisDepth,'fromRawData')
 end
 
 if strcmp(AnalysisDepth,'fromPreProcessedData')
-    load([DataDir,filesep,'combinedData.mat']);
+    load([DataDir,filesep,'combinedData.mat'],'cleandata');
 end
 
-%% 
+%% Fit models
 
-lengthOfSlidingWindow = 9; %Extra smoothing to simplify visual interpretation of instructive cartoon figures
-model = fitMixtureModel(cleandata,lengthOfSlidingWindow);
-moving_bias = model.moving_bias;
+if strcmp(AnalysisDepth,'fromRawData') || strcmp(AnalysisDepth,'fromPreProcessedData')
 
-whichFigures.MixMod_polar = true;
-plotMixtureModel(model,...
-    whichFigures,['F3_TCCModel_og_Input_',AnalysisDepth])
-plotMixtureModel(model,...
-    whichFigures,['F3_TCCModel_og_Input_',AnalysisDepth],false)
+    % % Fit a mixture model to the real data (so that we can replicate the bias)
 
-%% Generate a set of paramters for skewed gaussians that would create this data structure
+    lengthOfSlidingWindow = 9; % Extra smoothing to simplify visual interpretation of instructive cartoon figures
+    model = fitMixtureModel(cleandata,lengthOfSlidingWindow);
+    moving_bias = model.moving_bias;
 
-gaussianWidth = 25;
+    whichFigures.MixMod_polar = true;
+    plotMixtureModel(model,...
+        whichFigures,['F3_TCCModel_og_Input_',AnalysisDepth])
+    plotMixtureModel(model,...
+        whichFigures,['F3_TCCModel_og_Input_',AnalysisDepth],false)
 
-[~, OGdata] = GenerativeModel([],'offsetGaussians',moving_bias,...
-    'gaussianWidth',gaussianWidth,'nTrials',size(cleandata.trialdata.cues,1));
-OGdata.trialdata.chosen = OGdata.trialdata.chosen';
+    % % Generate simulated data that matches the bias in the real data
 
-save('OGdata.mat')
+    gaussianWidth = 25;
 
-OG_model = fitMixtureModel(OGdata,lengthOfSlidingWindow);
-OG_moving_bias = OG_model.moving_bias;
+    [~, OGdata] = GenerativeModel([],'offsetGaussians',moving_bias,...
+        'gaussianWidth',gaussianWidth,'nTrials',size(cleandata.trialdata.cues,1));
+    OGdata.trialdata.chosen = OGdata.trialdata.chosen';
+
+    OG_model = fitMixtureModel(OGdata,lengthOfSlidingWindow);
+
+    save('OGdata.mat','OGdata','OG_model')
+
+else
+    load('OGdata.mat')
+end
+
+%% Plot figures
 
 whichFigures.MixMod_polar = true;
 plotMixtureModel(OG_model,...
     whichFigures,['F3_TCCModel_og_Output_',AnalysisDepth])
 plotMixtureModel(OG_model,...
     whichFigures,['F3_TCCModel_og_Output_',AnalysisDepth],false)
-
-%%
 
 plotSimilarityMatrix(OGdata.trialdata.similarityMatrix,...
     'og','../')
