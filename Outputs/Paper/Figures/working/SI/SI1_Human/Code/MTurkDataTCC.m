878d52d65d8c9dd1f7b537bbea5e4025ed3bd5f8
Danny Garside <dannygarside@outlook.com>
2024-07-30 17:36:45 +0100

    MTurk TCC models

--- Outputs/Paper/Figures/working/SI/SI1_Human/Code/MTurkDataTCC.m
+++ Outputs/Paper/Figures/working/SI/SI1_Human/Code/MTurkDataTCC.m
@@ -10,7 +10,7 @@ clc, clear, close all
 % fromPreProcessedData:      % Generate figures from the pre-processed data
 % fromModelOutput:           % Generate figures from the model outputs only (fastest)
 
-AnalysisDepth = 'fromModelOutput';
+AnalysisDepth = 'fromPreProcessedData';
 
 %% Behind the scenes...
 
@@ -35,16 +35,59 @@ if strcmp(AnalysisDepth,'fromPreProcessedData')
     data.trialdata.nBig = 64;
     data.trialdata.nSmall = 4;
     data.trialdata.nTrials = size(data.trialdata.cues,1);
-
-    params = [1,0,0,0,0,0,0]; % telling the model that we want to fit a free similarity matrix model
+    
     rn = 0; % random number for reproducibility
-    dim1 = []; % parameters for dimensionality reduction, left empty here
-    dim2 = [];
 
-    x = ParameterEstimator(data,params,rn,dim1,dim2);
-
-    filename = ['MTurk_TCC-FreeSimilarityMatrix_',datestr(now,'yymmdd-HHMMSS')];
-    save([ModelDir,filename],'x')    
+%     params = [1,0,0,0,0,0,0]; % telling the model that we want to fit a free similarity matrix model
+%     dim1 = []; % parameters for dimensionality reduction, left empty here
+%     dim2 = [];
+% 
+%     x = ParameterEstimator(data,params,rn,dim1,dim2);
+% 
+%     filename = ['MTurk_TCC-FreeSimilarityMatrix_',datestr(now,'yymmdd-HHMMSS')];
+%     save([ModelDir,filename],'x')    
+    
+    %-% D-prime and gaussian width
+    
+    params = [0,1,0,0,1,0,0];
+    
+    
+    [x_dpgw,aic,bic,nll_x,x0] = ParameterEstimator(data,params,rn);
+    
+    filename = ['MTurk_TCC-dpgw_',datestr(now,'yymmdd-HHMMSS')];
+    save([ModelDir,filename],...
+        '-regexp', '^(?!(data)$).')
+    % save everything except data (https://www.mathworks.com/matlabcentral/answers/101287-how-do-i-save-all-of-the-workspace-variables-except-for-a-certain-specified-variable-name-in-matlab#answer_110635)
+    
+    disp(x_dpgw(1))
+    disp(x_dpgw(2))
+    
+    %-% SSNU model
+    
+    params = [0,0,0,1,0,0,0];
+    
+    [x_ssnu,aic,bic,nll_x,x0] = ParameterEstimator(data,params,rn,[],[],...
+        'dPrime',           x_dpgw(1),...
+        'gaussianWidth',    x_dpgw(2));
+    
+    filename = ['MTurk_TCC-ssnu_',datestr(now,'yymmdd-HHMMSS')];
+    save([ModelDir,filename],...
+        '-regexp', '^(?!(data)$).')
+    % save everything except data (https://www.mathworks.com/matlabcentral/answers/101287-how-do-i-save-all-of-the-workspace-variables-except-for-a-certain-specified-variable-name-in-matlab#answer_110635)
+     
+    %-% Offset gaussian model
+    
+    params = [0,0,0,0,0,0,1];
+    
+    [x_og,aic,bic,nll_x,x0] = ParameterEstimator(data,params,rn,[],[],...
+        'dPrime',           x_dpgw(1),...
+        'gaussianWidth',    x_dpgw(2));
+    
+    filename = ['MTurk_TCC-og_',datestr(now,'yymmdd-HHMMSS')];
+    save([ModelDir,filename],...
+        '-regexp', '^(?!(data)$).')
+    % save everything except data (https://www.mathworks.com/matlabcentral/answers/101287-how-do-i-save-all-of-the-workspace-variables-except-for-a-certain-specified-variable-name-in-matlab#answer_110635)
+    
 end
 
 if strcmp(AnalysisDepth,'fromModelOutput')
@@ -61,4 +104,24 @@ closestToZero = [7,30,38,61]; % from MTurkDataMM.m
 plotSimilarityMatrix(x,... 
     [filename,'_',num2str(closestToZero)],'../',...
     closestToZero,false) 
+
+%%
+
+nTrials = 46000;
+
+load('MTurk_TCC-dpgw_240730-172702.mat')
+% plotSimilarityMatrix(x, filename, '../')
+nParam = 2;
+[aic,bic] = aicbic(-nll_x,nParam,nTrials)
+
+load('MTurk_TCC-ssnu_240730-172824.mat')
+% plotSimilarityMatrix(x, filename, '../')
+nParam = 182;
+[aic,bic] = aicbic(-nll_x,nParam,nTrials)
+
+load('MTurk_TCC-og_240730-172857.mat')
+% plotSimilarityMatrix(x, filename, '../')
+nParam = 182;
+[aic,bic] = aicbic(-nll_x,nParam,nTrials)
+
 
